d07ad82c099b0968a0f91fbb6d04f77e366d7d4a
dota chuanqi <chuanqidota@126.com>
2025-02-06 02:49:53 +0000

    fix

--- app/k8s/model/k8sconfig.go
+++ app/k8s/model/k8sconfig.go
@@ -4,7 +4,7 @@ type K8SCluster struct {
 	BaseModel
 	ClusterName    string `json:"clusterName" gorm:"column:cluster_name;type:string;size:100;comment:集群名称" binding:"required"`
 	KubeConfig     string `json:"kubeConfig" gorm:"column:kube_config;type:string;size:12800;comment:集群凭证" binding:"required"`
-	ClusterVersion string `json:"clusterVersion" gorm:"comment:集群版本"`
+	ClusterVersion string `json:"clusterVersion" gorm:"column:cluster_version;type:string;size:100;comment:集群版本"`
 }
 
 func (K8SCluster) TableName() string {
