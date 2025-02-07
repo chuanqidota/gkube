912eda22b4047b7363c1e6c12547298ff0ca2da7
dota chuanqi <chuanqidota@126.com>
2025-02-07 10:10:06 +0000

    model-fix

--- models/k8s/node.go
+++ models/k8s/node.go
@@ -5,15 +5,15 @@ import (
 )
 
 type NodeInfo struct {
-	Name             string
-	Labels           map[string]string
-	IsReady          bool
-	Addresses        []corev1.NodeAddress
-	CapacityCPU      string
-	CapacityMemory   string
-	AllocatableCPU   string
-	AllocatableMem   string
-	OSImage          string
-	KernelVersion    string
-	ContainerRuntime string
+	Name             string `json:"name" label:"名称"` 
+	Labels           map[string]string `json:"labels" label:"标签"`
+	IsReady          bool `json:"is_ready" label:"是否就绪"`
+	Addresses        []corev1.NodeAddress `json:"addresses" label:"地址"`
+	CapacityCPU      string `json:"capacity_cpu" label:"总cpu"`
+	CapacityMemory   string `json:"capacity_memory" label:"总内存"`
+	AllocatableCPU   string `json:"allocatable_cpu" label:"可用cpu"`
+	AllocatableMem   string `json:"allocatable_mem" label:"可用内存"`
+	OSImage          string `json:"os_image" label:"操作系统"`
+	KernelVersion    string `json:"kernel_version" label:"内核版本"`
+	ContainerRuntime string `json:"container_runtime" label:"容器运行时"`
 }
