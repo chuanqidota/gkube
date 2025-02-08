cf12800ea70f682a0b770d5e75eb2d9a1837921a
dota chuanqi <chuanqidota@126.com>
2025-02-08 03:06:57 +0000

    namespace

--- pkg/k8s/namespace/api.go
+++ pkg/k8s/namespace/api.go
@@ -1,1 +1,17 @@
 package namespace
+
+import (
+	"context"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+func GetNamespaceList(client *kubernetes.Clientset) (*corev1.NamespaceList, error) {
+	namespace, err := client.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return namespace, err
+}
