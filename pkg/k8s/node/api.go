6a2111191abd6e490af233b324856f8fce0d2bff
dota chuanqi <chuanqidota@126.com>
2025-02-08 01:57:30 +0000

    node-yaml

--- pkg/k8s/node/api.go
+++ pkg/k8s/node/api.go
@@ -1,1 +1,26 @@
 package node
+
+import (
+	"context"
+	"encoding/json"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"sigs.k8s.io/yaml"
+)
+
+func GetNodeYaml(client *kubernetes.Clientset, nodeName string) (string, error) {
+	node, err := client.CoreV1().Nodes().Get(context.TODO(), nodeName, metav1.GetOptions{})
+	if err != nil {
+		return "", err
+	}
+	nodeJSON, err := json.Marshal(node)
+	if err != nil {
+		return "", err
+	}
+	nodeYAML, err := yaml.JSONToYAML(nodeJSON)
+	if err != nil {
+		return "", err
+	}
+	return string(nodeYAML), nil
+}
