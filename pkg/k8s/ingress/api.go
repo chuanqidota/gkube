e5b953cf9ab233e8a55d3da6b697bbb57dae5b59
dota <76216654+chuanqidota@users.noreply.github.com>
2025-02-10 23:34:17 +0800

    feat(k8s/ingress): 添加 Ingress 资源的获取、创建、更新和删除功能
    - 新增 GetIngressList 函数，用于获取 Ingress列表
    - 新增 GetIngressYaml 函数，用于获取 Ingress YAML
    - 新增 CreateIngress 函数，用于创建 Ingress 资源
    - 新增 UpdateIngress 函数，用于更新 Ingress 资源- 新增 DeleteIngressByName函数，用于按名称删除 Ingress 资源
    - 新增 DeleteIngressByLabel 函数，用于按标签删除 Ingress资源
    - 新增 DeleteIngressByField 函数，用于按字段删除 Ingress 资源

--- pkg/k8s/ingress/api.go
+++ pkg/k8s/ingress/api.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/labels"
 
 	netv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -11,6 +13,13 @@ import (
 	"sigs.k8s.io/yaml"
 )
 
+// GetIngressList
+//
+//	@Description: 获取ingress列表
+//	@param client
+//	@param namespace
+//	@return []netv1.Ingress
+//	@return error
 func GetIngressList(client *kubernetes.Clientset, namespace string) ([]netv1.Ingress, error) {
 	ingress, err := client.NetworkingV1().Ingresses(namespace).List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
@@ -19,6 +28,14 @@ func GetIngressList(client *kubernetes.Clientset, namespace string) ([]netv1.Ing
 	return ingress.Items, nil
 }
 
+// GetIngressYaml
+//
+//	@Description: 获取ingress yaml
+//	@param client
+//	@param namespace
+//	@param name
+//	@return string
+//	@return error
 func GetIngressYaml(client *kubernetes.Clientset, namespace, name string) (string, error) {
 	ingress, err := client.NetworkingV1().Ingresses(namespace).Get(context.TODO(), name, metav1.GetOptions{})
 	if err != nil {
@@ -35,27 +52,37 @@ func GetIngressYaml(client *kubernetes.Clientset, namespace, name string) (strin
 	return string(configmapYAML), nil
 }
 
+// CreateIngress
+//
+//	@Description: 创建ingress
+//	@param client
+//	@param namespace
+//	@param name
+//	@param host
+//	@param path
+//	@param serviceName
+//	@param servicePort
+//	@return bool
+//	@return error
 func CreateIngress(client *kubernetes.Clientset, namespace, name, host, path, serviceName, servicePort string) (bool, error) {
-	ingressYAML := fmt.Sprintf(
-		`
-apiVersion: networking.k8s.io/v1
-kind: Ingress
-metadata:
-  name: %s
-  namespace: %s
-spec:
-  rules:
-  - host: %s
-    http:
-      paths:
-      - path: %s
-        pathType: Prefix
-        backend:
-          service:
-            name: %s
-            port:
-              number: %s
-        
+	ingressYAML := fmt.Sprintf(`
+	apiVersion: networking.k8s.io/v1
+	kind: Ingress
+	metadata:
+	  name: %s
+	  namespace: %s
+	spec:
+	  rules:
+	  - host: %s
+		http:
+		  paths:
+		  - path: %s
+			pathType: Prefix
+			backend:
+			  service:
+				name: %s
+				port:
+				  number: %s
         `, name, namespace, host, path, serviceName, servicePort)
 	var ingress netv1.Ingress
 	if err := yaml.Unmarshal([]byte(ingressYAML), &ingress); err != nil {
@@ -63,8 +90,72 @@ spec:
 	}
 	_, err := client.NetworkingV1().Ingresses(namespace).Create(context.TODO(), &ingress, metav1.CreateOptions{})
 	if err != nil {
-		return false, fmt.Errorf("创建ingress资源失败:%s",err.Error())
+		return false, fmt.Errorf("创建ingress资源失败:%s", err.Error())
 	}
-    return true,nil
+	return true, nil
+}
 
+func UpdateIngress(client *kubernetes.Clientset, ingressYaml string) (bool, error) {
+	var ingress netv1.Ingress
+	if err := yaml.Unmarshal([]byte(ingressYaml), &ingress); err != nil {
+		return false, fmt.Errorf("yaml文件错误:%s", err.Error())
+	}
+	_, err := client.NetworkingV1().Ingresses(ingress.Namespace).Update(context.TODO(), &ingress, metav1.UpdateOptions{})
+	if err != nil {
+		return false, fmt.Errorf("更新ingress资源失败:%s", err.Error())
+	}
+	return true, nil
+}
+
+// DeleteIngressByName
+//
+//	@Description: 删除ingress通过名称
+//	@param client
+//	@param namespace
+//	@param name
+//	@return error
+func DeleteIngressByName(client *kubernetes.Clientset, namespace, name string) error {
+	err := client.NetworkingV1().Ingresses(namespace).Delete(context.TODO(), name, metav1.DeleteOptions{})
+	if err != nil {
+		return fmt.Errorf("删除ingress资源失败:%s", err.Error())
+	}
+	return nil
+}
+
+// DeleteIngressByLabel
+//
+//	@Description: 删除ingress通过标签
+//	@param client
+//	@param namespace
+//	@param labelMap
+//	@return bool
+//	@return error
+func DeleteIngressByLabel(client *kubernetes.Clientset, namespace string, labelMap map[string]string) (bool, error) {
+	labelSelector := labels.SelectorFromSet(labelMap) // 创建标签选择器
+	err := client.NetworkingV1().Ingresses(namespace).DeleteCollection(context.TODO(), metav1.DeleteOptions{}, metav1.ListOptions{
+		LabelSelector: labelSelector.String(),
+	})
+	if err != nil {
+		return false, fmt.Errorf("删除ingress资源失败:%s", err.Error())
+	}
+	return true, nil
+}
+
+// DeleteIngressByField
+//
+//	@Description: 删除ingress通过字段
+//	@param client
+//	@param namespace
+//	@param fieldMap
+//	@return bool
+//	@return error
+func DeleteIngressByField(client *kubernetes.Clientset, namespace string, fieldMap map[string]string) (bool, error) {
+	fieldSelector := fields.SelectorFromSet(fieldMap) // 创建标签选择器
+	err := client.NetworkingV1().Ingresses(namespace).DeleteCollection(context.TODO(), metav1.DeleteOptions{}, metav1.ListOptions{
+		FieldSelector: fieldSelector.String(),
+	})
+	if err != nil {
+		return false, fmt.Errorf("删除ingress资源失败:%s", err.Error())
+	}
+	return true, nil
 }
