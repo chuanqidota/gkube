1f91725cddda853ad6a95fd7c6b8526b3e9b405c
dota chuanqi <chuanqidota@126.com>
2025-02-10 11:04:25 +0000

    ingress-get

---
+++ pkg/k8s/ingress/api.go
@@ -0,0 +1,35 @@
+package ingress
+
+import (
+	"context"
+	"encoding/json"
+
+	netv1 "k8s.io/api/networking/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"sigs.k8s.io/yaml"
+)
+
+func GetIngressList(client *kubernetes.Clientset, namespace string) ([]netv1.Ingress, error) {
+	ingress, err := client.NetworkingV1().Ingresses(namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return ingress.Items, nil
+}
+
+func GetIngressYaml(client *kubernetes.Clientset, namespace, name string) (string, error) {
+	ingress, err := client.NetworkingV1().Ingresses(namespace).Get(context.TODO(), name, metav1.GetOptions{})
+	if err != nil {
+		return "", nil
+	}
+	ingressJSON, err := json.Marshal(ingress)
+	if err != nil {
+		return "", err
+	}
+	configmapYAML, err := yaml.JSONToYAML(ingressJSON)
+	if err != nil {
+		return "", err
+	}
+	return string(configmapYAML), nil
+}
