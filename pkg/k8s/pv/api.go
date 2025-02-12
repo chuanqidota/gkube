908d50d26ef21fc01a0fd6a47f58e67d9fa4be6a
dota <76216654+chuanqidota@users.noreply.github.com>
2025-02-12 22:53:41 +0800

    feat(k8s): 添加 PV 和 PVC 的 API 接口
    - 新增了 PV 和 PVC 相关的多个 API 函数，包括获取列表、创建、更新、删除等操作
    - PV API 包含按名称、标签、字段获取 PV 的功能
    - PVC API包含按名称、标签、字段获取 PVC 的功能
    - 所有 API 函数都遵循了 Kubernetes 客户端的标准调用方式

--- pkg/k8s/pv/api.go
+++ pkg/k8s/pv/api.go
@@ -1,1 +1,187 @@
 package pv
+
+import (
+	"context"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/kubernetes"
+)
+
+// GetPVList
+//
+//	@Description: 获取PV列表
+//	@param client
+//	@return []corev1.PersistentVolume
+//	@return error
+func GetPVList(client *kubernetes.Clientset) ([]corev1.PersistentVolume, error) {
+	pvList, err := client.CoreV1().PersistentVolumes().List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return pvList.Items, nil
+}
+
+// GetPVByName
+//
+//	@Description: 根据名称获取PV
+//	@param client
+//	@param name
+//	@return *corev1.PersistentVolume
+//	@return error
+func GetPVByName(client *kubernetes.Clientset, name string) (*corev1.PersistentVolume, error) {
+	pv, err := client.CoreV1().PersistentVolumes().Get(context.Background(), name, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return pv, nil
+}
+
+// GetPVByLabel
+//
+//	@Description: 根据标签获取PV
+//	@param client
+//	@param labelMap
+//	@return []corev1.PersistentVolume
+//	@return error
+func GetPVByLabel(client *kubernetes.Clientset, labelMap map[string]string) ([]corev1.PersistentVolume, error) {
+	labelSelector := labels.SelectorFromSet(labelMap)
+	pvList, err := client.CoreV1().PersistentVolumes().List(context.Background(), metav1.ListOptions{
+		LabelSelector: labelSelector.String(),
+	})
+	if err != nil {
+		return nil, err
+	}
+	return pvList.Items, nil
+}
+
+// GetPVByField
+//
+//	@Description: 根据字段获取PV
+//	@param client
+//	@param fieldMap
+//	@return []corev1.PersistentVolume
+//	@return error
+func GetPVByField(client *kubernetes.Clientset, fieldMap map[string]string) ([]corev1.PersistentVolume, error) {
+	fieldSelector := fields.SelectorFromSet(fieldMap)
+	pvList, err := client.CoreV1().PersistentVolumes().List(context.Background(), metav1.ListOptions{
+		FieldSelector: fieldSelector.String(),
+	})
+	if err != nil {
+		return nil, err
+	}
+	return pvList.Items, nil
+}
+
+// GetPVYaml
+//
+//	@Description: 获取PV Yaml
+//	@param client
+//	@param name
+//	@return string
+//	@return error
+func GetPVYaml(client *kubernetes.Clientset, name string) (string, error) {
+	pv, err := client.CoreV1().PersistentVolumes().Get(context.Background(), name, metav1.GetOptions{})
+	if err != nil {
+		return "", err
+	}
+	return pv.String(), nil
+}
+
+// CreatePV
+//
+//	@Description: 创建PV
+//	@param client
+//	@param pvYaml
+//	@return bool
+//	@return error
+func CreatePV(client *kubernetes.Clientset, pvYaml string) (bool, error) {
+
+	pv, err := client.CoreV1().PersistentVolumes().Create(context.Background(), &corev1.PersistentVolume{}, metav1.CreateOptions{})
+	if err != nil {
+		return false, err
+	}
+	return pv.Name == pvYaml, nil
+}
+
+// UpdatePV
+//
+//	@Description: 更新PV
+//	@param client
+//	@param pvYaml
+//	@return bool
+//	@return error
+func UpdatePV(client *kubernetes.Clientset, pvYaml string) (bool, error) {
+	pv, err := client.CoreV1().PersistentVolumes().Update(context.Background(), &corev1.PersistentVolume{}, metav1.UpdateOptions{})
+	if err != nil {
+		return false, err
+	}
+	return pv.Name == pvYaml, nil
+}
+
+// DeletePV
+//
+//	@Description: 删除PV
+//	@param client
+//	@param name
+//	@return bool
+//	@return error
+func DeletePV(client *kubernetes.Clientset, name string) (bool, error) {
+	err := client.CoreV1().PersistentVolumes().Delete(context.Background(), name, metav1.DeleteOptions{})
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+// DeletePVByLabel
+//
+//	@Description: 删除PV通过标签
+//	@param client
+//	@param labelMap
+//	@return bool
+//	@return error
+func DeletePVByLabel(client *kubernetes.Clientset, labelMap map[string]string) (bool, error) {
+	labelSelector := labels.SelectorFromSet(labelMap)
+	err := client.CoreV1().PersistentVolumes().DeleteCollection(context.Background(), metav1.DeleteOptions{}, metav1.ListOptions{
+		LabelSelector: labelSelector.String(),
+	})
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+// DeletePVByField
+//
+//	@Description: 删除PV通过字段
+//	@param client
+//	@param fieldMap
+//	@return bool
+//	@return error
+func DeletePVByField(client *kubernetes.Clientset, fieldMap map[string]string) (bool, error) {
+	fieldSelector := fields.SelectorFromSet(fieldMap)
+	err := client.CoreV1().PersistentVolumes().DeleteCollection(context.Background(), metav1.DeleteOptions{}, metav1.ListOptions{
+		FieldSelector: fieldSelector.String(),
+	})
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+// DeletePVByName
+//
+//	@Description: 删除PV通过名称
+//	@param client
+//	@param name
+//	@return bool
+//	@return error
+func DeletePVByName(client *kubernetes.Clientset, name string) (bool, error) {
+	err := client.CoreV1().PersistentVolumes().Delete(context.Background(), name, metav1.DeleteOptions{})
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
--- pkg/k8s/pvc/api.go
+++ pkg/k8s/pvc/api.go
@@ -1,1 +1,170 @@
 package pvc
+
+import (
+	"context"
+	"fmt"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/kubernetes"
+	"sigs.k8s.io/yaml"
+)
+
+// GetPVCList
+//
+//	@Description: 获取PVC列表
+//	@param client
+//	@param namespace
+//	@return []corev1.PersistentVolumeClaim
+//	@return error
+func GetPVCList(client *kubernetes.Clientset, namespace string) ([]corev1.PersistentVolumeClaim, error) {
+	pvcList, err := client.CoreV1().PersistentVolumeClaims(namespace).List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return pvcList.Items, nil
+}
+
+// GetPVCByName
+//
+//	@Description: 根据名称获取PVC
+//	@param client
+//	@param namespace
+//	@param name
+//	@return *corev1.PersistentVolumeClaim
+//	@return error
+func GetPVCByName(client *kubernetes.Clientset, namespace, name string) (*corev1.PersistentVolumeClaim, error) {
+	pvc, err := client.CoreV1().PersistentVolumeClaims(namespace).Get(context.Background(), name, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return pvc, nil
+}
+
+// GetPVCByLabel
+//
+//	@Description: 根据标签获取PVC
+//	@param client
+//	@param namespace
+//	@param labelMap
+//	@return []corev1.PersistentVolumeClaim
+//	@return error
+func GetPVCByLabel(client *kubernetes.Clientset, namespace string, labelMap map[string]string) ([]corev1.PersistentVolumeClaim, error) {
+	labelSelector := labels.SelectorFromSet(labelMap)
+	pvcList, err := client.CoreV1().PersistentVolumeClaims(namespace).List(context.Background(), metav1.ListOptions{
+		LabelSelector: labelSelector.String(),
+	})
+	if err != nil {
+		return nil, err
+	}
+	return pvcList.Items, nil
+}
+
+// GetPVCByField
+//
+//	@Description: 根据字段获取PVC
+//	@param client
+//	@param namespace
+//	@param fieldMap
+//	@return []corev1.PersistentVolumeClaim
+//	@return error
+func GetPVCByField(client *kubernetes.Clientset, namespace string, fieldMap map[string]string) ([]corev1.PersistentVolumeClaim, error) {
+	fieldSelector := labels.SelectorFromSet(fieldMap)
+	pvcList, err := client.CoreV1().PersistentVolumeClaims(namespace).List(context.Background(), metav1.ListOptions{
+		FieldSelector: fieldSelector.String(),
+	})
+	if err != nil {
+		return nil, err
+	}
+	return pvcList.Items, nil
+}
+
+// GetPVCYaml
+//
+//	@Description: 获取PVC Yaml
+//	@param client
+//	@param namespace
+//	@param name
+//	@return string
+//	@return error
+func GetPVCYaml(client *kubernetes.Clientset, namespace, name string) (string, error) {
+	pvc, err := client.CoreV1().PersistentVolumeClaims(namespace).Get(context.Background(), name, metav1.GetOptions{})
+	if err != nil {
+		return "", err
+	}
+	return pvc.String(), nil
+}
+
+// CreatePVC
+//
+//	@Description: 创建PVC
+//	@param client
+//	@param namespace
+//	@param pvcYaml
+//	@return bool
+//	@return error
+func CreatePVC(client *kubernetes.Clientset, namespace, pvcYaml string) (bool, error) {
+	var pvc corev1.PersistentVolumeClaim
+	if err := yaml.Unmarshal([]byte(pvcYaml), &pvc); err != nil {
+		return false, fmt.Errorf("yaml文件错误:%s", err.Error())
+	}
+	_, err := client.CoreV1().PersistentVolumeClaims(namespace).Create(context.Background(), &pvc, metav1.CreateOptions{})
+	if err != nil {
+		return false, fmt.Errorf("创建pvc资源失败:%s", err.Error())
+	}
+	return true, nil
+}
+
+// DeletePVCByName
+//
+//	@Description: 删除PVC
+//	@param client
+//	@param namespace
+//	@param name
+//	@return bool
+//	@return error
+func DeletePVCByName(client *kubernetes.Clientset, namespace, name string) (bool, error) {
+	err := client.CoreV1().PersistentVolumeClaims(namespace).Delete(context.Background(), name, metav1.DeleteOptions{})
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+// DeletePVCByLabel
+//
+//	@Description: 删除PVC
+//	@param client
+//	@param namespace
+//	@param labelMap
+//	@return bool
+//	@return error
+func DeletePVCByLabel(client *kubernetes.Clientset, namespace string, labelMap map[string]string) (bool, error) {
+	labelSelector := labels.SelectorFromSet(labelMap)
+	err := client.CoreV1().PersistentVolumeClaims(namespace).DeleteCollection(context.Background(), metav1.DeleteOptions{}, metav1.ListOptions{
+		LabelSelector: labelSelector.String(),
+	})
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+// DeletePVCByField
+//
+//	@Description: 删除PVC
+//	@param client
+//	@param namespace
+//	@param fieldMap
+//	@return bool
+//	@return error
+func DeletePVCByField(client *kubernetes.Clientset, namespace string, fieldMap map[string]string) (bool, error) {
+	fieldSelector := labels.SelectorFromSet(fieldMap)
+	err := client.CoreV1().PersistentVolumeClaims(namespace).DeleteCollection(context.Background(), metav1.DeleteOptions{}, metav1.ListOptions{
+		FieldSelector: fieldSelector.String(),
+	})
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
