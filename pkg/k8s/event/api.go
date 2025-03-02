be08fe8551c3dd4e3be33823e8978b3396d84fa0
dota <76216654+chuanqidota@users.noreply.github.com>
2025-03-02 16:03:57 +0800

    feat(k8s): 添加事件相关 API 函数
    - 新增 GetEventList 函数，用于获取指定命名空间下的事件列表
    - 新增 GetPodsEventList 函数，用于根据 pod 名称获取事件列表
    - 在 pkg/k8s 目录下创建新的 log子包

--- pkg/k8s/event/api.go
+++ pkg/k8s/event/api.go
@@ -1,1 +1,41 @@
 package event
+
+import (
+	"context"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+// GetEventList
+//
+//	@Description: 获取事件列表
+//	@param client
+//	@param namespace
+//	@return []corev1.Event
+//	@return error
+func GetEventList(client *kubernetes.Clientset, namespace string) ([]corev1.Event, error) {
+	eventList, err := client.CoreV1().Events(namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return eventList.Items, nil
+}
+
+// GetPodsEventList
+//
+//	@Description: 根据pod名称获取事件列表
+//	@param client
+//	@param namespace
+//	@param podName
+//	@return []corev1.Event
+//	@return error
+func GetPodsEventList(client *kubernetes.Clientset, namespace, podName string) ([]corev1.Event, error) {
+	eventList, err := client.CoreV1().Events(namespace).List(context.TODO(), metav1.ListOptions{
+		FieldSelector: "involvedObject.name=" + podName,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return eventList.Items, nil
+}
