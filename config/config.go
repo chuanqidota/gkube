13db0140cb5a2070e231848bb123b49d0274c506
dota <76216654+chuanqidota@users.noreply.github.com>
2025-04-05 08:13:19 +0800

    feat(config): 添加配置文件并优化配置结构
    - 为 Server、Database、Redis、ElasticSearch、Audit 和 S3 结构体添加 json 标签
    - 新增 config.yaml 配置文件，包含所有必要的配置项
    - 重构 Config 结构体，使其更易于管理和扩展

--- config/config.go
+++ config/config.go
@@ -12,34 +12,34 @@ type Config struct {
 	Server struct {
 		Ip   string `json:"ip"`
 		Port string `json:"port"`
-	}
+	} `json:"server"`
 	Database struct {
 		User     string `json:"user"`
 		Password string `json:"password"`
 		Host     string `json:"host"`
 		Port     int    `json:"port"`
 		Name     string `json:"name"`
-	}
+	} `json:"database"`
 	Redis struct {
 		Addr     string `json:"addr"`
 		Password string `json:"password"`
 		DB       int    `json:"db"`
-	}
+	} `json:"redis"`
 	ElasticSearch struct {
 		Url      string `json:"url" comment:"es地址"`
 		Username string `json:"username" comment:"es用户名"`
 		Password string `json:"password" comment:"es密码"`
-	}
+	} `json:"elasticSearch"`
 	Audit struct {
 		RecordAuditIndex string `json:"record_audit" comment:"操作审计-es索引"`
-	}
+	} `json:"audit"`
 	S3 struct {
 		EndPoint        string `json:"endpoint" comment:"地址"`
 		AccessKeyID     string `json:"accessKeyID" comment:"密钥ID"`
 		SecretAccessKey string `json:"secretAccessKey" comment:"密钥KEY"`
 		UseSSL          bool   `json:"useSSL" comment:"是否使用SSL"`
 		Bucket          string `json:"bucket" comment:"桶名字"`
-	}
+	} `json:"s3"`
 }
 
 var Conf = new(Config)
