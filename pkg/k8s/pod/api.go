aaffae2c25fe566dd915a2b071c9e3ef83b60bdc
dota chuanqi <chuanqidota@126.com>
2025-06-14 08:53:01 +0000

    feat(pod): add function to get latest pod list by deployment
    - Implement GetLatestPodList function to retrieve the latest pod list associated with a specific deployment
    - Add logic to fetch and filter ReplicaSets belonging to the specified Deployment
    - Sort ReplicaSets by revision to find the latest one
    - Retrieve and return the pod list managed by the latest ReplicaSet

--- pkg/k8s/pod/api.go
+++ pkg/k8s/pod/api.go
@@ -2,6 +2,11 @@ package pod
 
 import (
 	"context"
+	"errors"
+	"sort"
+	"strconv"
+
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
@@ -183,4 +188,58 @@ func DeletePodByLabel(client *kubernetes.Clientset, namespace string, labelMap m
 		return err
 	}
 	return nil
+}
+
+// GetLatestPodList 获取最新的pod列表
+
+func GetLatestPodList(client *kubernetes.Clientset, namespace, deployment string) ([]corev1.Pod, error) {
+	deploy, err := client.AppsV1().Deployments(namespace).Get(context.TODO(), deployment, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+	// 获取该 Deployment 关联的所有 ReplicaSet
+	rsList, err := client.AppsV1().ReplicaSets(namespace).List(
+		context.TODO(), metav1.ListOptions{
+			LabelSelector: metav1.FormatLabelSelector(deploy.Spec.Selector),
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+	// 过滤出属于该 Deployment 的 ReplicaSet
+	var ownedRS []appsv1.ReplicaSet
+	for _, rs := range rsList.Items {
+		for _, ownerRef := range rs.OwnerReferences {
+			if ownerRef.UID == deploy.UID {
+				ownedRS = append(ownedRS, rs)
+				break
+			}
+		}
+	}
+	if len(ownedRS) == 0 {
+		return nil, errors.New("未找到属于该 Deployment 的 ReplicaSet")
+	}
+
+	getRevision := func(rs appsv1.ReplicaSet) int64 {
+		revision := rs.Annotations["deployment.kubernetes.io/revision"]
+		if revision == "" {
+			return 0
+		}
+		revisionInt, _ := strconv.Atoi(revision)
+		return int64(revisionInt)
+	}
+
+	// 按 Revision 排序，获取最新的 ReplicaSet
+	sort.Slice(ownedRS, func(i, j int) bool {
+		return getRevision(ownedRS[i]) > getRevision(ownedRS[j])
+	})
+	latestRS := ownedRS[0]
+
+	// 获取该 ReplicaSet 管理的 Pod 的 IP 地址
+	podList, err := client.CoreV1().Pods(namespace).List(
+		context.TODO(), metav1.ListOptions{
+			LabelSelector: metav1.FormatLabelSelector(latestRS.Spec.Selector),
+		},
+	)
+	return podList.Items, err
 }
